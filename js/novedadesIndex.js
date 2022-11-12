5b704c942e7cf46f65e46ad1248f893c4e74117f
Eric Aguerreberry <eric_aguerreberry@hotmail.com>
2022-11-11 21:52:39 -0300

    Novedades Index arreglado

--- js/novedadesIndex.js
+++ js/novedadesIndex.js
@@ -6,8 +6,10 @@ const client = contentful.createClient({
 const d = document,
 w = window,
 $posts = d.getElementById("posts"),
+$postsDesktop = d.getElementById("posts-desktop"),
 $loader = d.querySelector(".loader"),
 $template = d.getElementById("post-template").content,
+$templateDesktop = d.getElementById("post-template-desktop").content,
 $fragment = d.createDocumentFragment();
 
 async function getPosts(){
@@ -15,18 +17,35 @@ async function getPosts(){
     try{
         const contentful = await client.getEntries({content_type:'noticia', limit:3});
         let posts = contentful.items;
-        posts.forEach(el=>{
-            $template.querySelector(".post-image").src = el.fields.foto.fields.file.url;
-            $template.querySelector(".post-image").alt = el.fields.title;
-            $template.querySelector(".post-title").innerHTML = el.fields.title;
-            $template.querySelector(".fecha-news").innerHTML = `
-            <figcaption>${new Date(el.fields.fecha).toLocaleDateString()}</figcaption>`;
-            $template.querySelector(".post-content").innerHTML = `<a href="./pages/singleNovedad.html?verinfo=${el.sys.id}">Leer publicación<i class="fas fa-chevron-right"></i></a>`;
-            let $clone = d.importNode($template, true);
-            $fragment.appendChild($clone);
-        });
-        $posts.appendChild($fragment);
-        $loader.style.display = "none";
+        if(w.screen.width<1024){
+            posts.forEach(el=>{
+                $template.querySelector(".post-image").src = el.fields.foto.fields.file.url;
+                $template.querySelector(".post-image").alt = el.fields.title;
+                $template.querySelector(".post-title").innerHTML = el.fields.title;
+                $template.querySelector(".fecha-news").innerHTML = `
+                <figcaption>${new Date(el.fields.fecha).toLocaleDateString()}</figcaption>`;
+                $template.querySelector(".post-content").innerHTML = `<a href="./pages/singleNovedad.html?verinfo=${el.sys.id}">Leer publicación<i class="fas fa-chevron-right"></i></a>`;
+                let $clone = d.importNode($template, true);
+                $fragment.appendChild($clone);
+            });
+            $posts.appendChild($fragment);
+            $loader.style.display = "none";
+        }
+        else{
+            posts.forEach(el=>{
+                $templateDesktop.querySelector(".post-image").src = el.fields.foto.fields.file.url;
+                $templateDesktop.querySelector(".post-image").alt = el.fields.title;
+                $templateDesktop.querySelector(".post-title").innerHTML = el.fields.title;
+                $templateDesktop.querySelector(".fecha-news").innerHTML = `
+                <figcaption>${new Date(el.fields.fecha).toLocaleDateString()}</figcaption>`;
+                $templateDesktop.querySelector(".post-content").innerHTML = `<a href="./pages/singleNovedad.html?verinfo=${el.sys.id}">Leer publicación<i class="fas fa-chevron-right"></i></a>`;
+                let $clone = d.importNode($templateDesktop, true);
+                $fragment.appendChild($clone);
+            });
+            $postsDesktop.appendChild($fragment);
+            $loader.style.display = "none";
+        }
+
     }
     catch (error){
         console.log(error);
